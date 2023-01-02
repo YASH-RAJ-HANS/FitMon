93ea04d5fedf136af8ec8219ef4144d739862c50
Afzal Khan <ak341668@gmail.com>
2023-01-02 21:44:23 +0530

    dev branch created

--- routes/userData.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {getUserData} = require('../controller/updateCOntroller.js')
-
-
-router.route('/:email').get(getUserData)
-
-
-
-module.exports = router;
--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {registerUser, loginUser} = require('../controller/userControllers')
-
-
-router.route('/register').post(registerUser)
-
-router.route('/login').post(loginUser)
-
-module.exports = router;
